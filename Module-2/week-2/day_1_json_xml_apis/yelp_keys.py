9ffff8c56a913d67dedc2080a56819e3491e6cae
nicolej <msyli1517@gmail.com>
2019-08-06 12:33:40 -0400

    new lectures added

--- Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
+++ Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
@@ -1,11 +1,11 @@
 """stores the yelp api client id and api key in a separate file for security and best practice"""
 
 def return_api_key():
-"""returns api key for yelp api"""
-    api_key = ''"
+    """returns api key for yelp api"""
+    api_key = 'Y5WhLpn_Ewth3-ATZgOFJ59ta1i7iA_Li_rFhWAhQiK5xrKj7F8o563WbjoUTCelbMICvdPcb2PqYONYkHW4hNtaA_aA14Eu8LiuFAw34wzW7TTSkAqo4MFQFGlIXXYx'
     return api_key
 
 def return_client_id():
-"""returns client id for yelp api"""
-    client_id = ''
+    """returns client id for yelp api"""
+    client_id = 'QwLLoiVef84YPA4Qr_Po1g'
     return client_id
